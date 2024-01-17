a4822c50e96d57217ecba09663ada1b6c8071ee7
roberto zopelaro <robertocarlosfolly23@gmail.com>
2024-01-17 21:37:20 +0000

    Colocando em prática

---
+++ index.js
@@ -0,0 +1,18 @@
+let Telefone = Number (prompt('Digite o numero do seu telefone'))
+  
+        switch(Telefone){
+            case 21:
+                        console.log('O numero esta correto')
+                        console.log('segunda linha')
+                        break;
+}
+      let idade = Number (prompt('Digite a idade do seu telefone'))
+      switch(idade){
+        case 15:
+                    console.log('A idade esta correta')
+                    console.log('segunda linha')
+                    break;
+                default: 
+                console.log('defalut')
+                break
+      }
